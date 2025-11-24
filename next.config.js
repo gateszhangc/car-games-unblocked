a4fd995ea4d7148f12e6810a729250aaa3165845
gateszhangc <gateszhang92@gmail.com>
2025-11-24 15:54:03 +0800

    解决404

--- next.config.js
+++ next.config.js
@@ -1,18 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // 确保 data 目录被包含在构建中
-  experimental: {
-    outputFileTracingIncludes: {
-      '/': ['./data/**/*'],
-    },
-  },
-  // 禁用 TypeScript 和 ESLint 校验
+  // 禁用 TypeScript 校验
   typescript: {
     ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
   },
 };
 
