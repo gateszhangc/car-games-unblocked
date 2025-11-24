1356be96b09dc987c017370e5bb1335f8fa5f315
gateszhangc <129406470+gateszhangc@users.noreply.github.com>
2025-11-24 15:44:52 +0800

    Update file path for loading HTML fragments

--- lib/loadHtmlFragment.ts
+++ lib/loadHtmlFragment.ts
@@ -2,7 +2,7 @@ import fs from 'fs';
 import path from 'path';
 
 export function loadHtmlFragment(filename: string): string {
-  const filePath = path.join(process.cwd(), 'data', filename);
+  const filePath = path.join(process.cwd(), ''public/data'', filename);
   if (fs.existsSync(filePath)) {
     return fs.readFileSync(filePath, 'utf8');
   }
